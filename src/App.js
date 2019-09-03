9c9fd9fcf28f0cd47798ab45cb98872164df954a
AnastasiiaBlaha <anastasiia.blaha@gmail.com>
2019-09-03 17:13:39 +0100

    added login functionality, routing (login and homepage)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
+import { BrowserRouter, Route } from 'react-router-dom';
 import SignUp from "./components/SignUp";
+import LogIn from "./components/LogIn";
 
 class App extends React.Component {
   state = {};
@@ -8,24 +10,43 @@ class App extends React.Component {
     console.log("now about to post the thing");
 
     fetch("https://jinder-backend.herokuapp.com/users", {
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body:  JSON.stringify({"user":
-            {
-              "email": state.email,
-              "password": state.password
-            }
-          })
-  })
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body:  JSON.stringify({"user":
+        {
+          "email": state.email,
+          "password": state.password
+        }
+      })
+    });
+  };
 
-}
+  createSession = state => {
+    console.log("now about to post the thing");
+
+    fetch("https://jinder-backend.herokuapp.com/api/sessions", {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body:  JSON.stringify({"user":
+        {
+          "email": state.email,
+          "password": state.password
+        }
+      })
+    });
+  };
 
   render() {
     return (
       <div className="App">
-        <SignUp createUser={this.createUser}></SignUp>
+          <BrowserRouter>
+            <Route exact path= "/" render={(props) => <SignUp {...props} createUser={this.createUser} />} />
+            <Route exact path= "/login" render={(props) => <LogIn {...props} createSession={this.createSession} />} />
+          </BrowserRouter>
       </div>
     );
   }
