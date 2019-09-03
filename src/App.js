eb5662a803c3c326e4080303fd586e76ffd03c7e
Student <ziwei.polaris@gmail.com>
2019-09-03 15:26:36 +0100

    can create a user on the front end

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
 import SignUp from "./components/SignUp";
 
-function App() {
-  return (
-    <div className="App">
-      <SignUp></SignUp>
-    </div>
-  );
+class App extends React.Component {
+  state = {};
+
+  createUser = state => {
+    console.log("now about to post the thing");
+
+    fetch("https://jinder-backend.herokuapp.com/users", {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body:  JSON.stringify({"user":
+            {
+              "email": state.email,
+              "password": state.password
+            }
+          })
+  })
+
+}
+
+  render() {
+    return (
+      <div className="App">
+        <SignUp createUser={this.createUser}></SignUp>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -4,22 +4,66 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 class SignUp extends React.Component {
+  state = {
+    email: null,
+    password: null,
+    passwordConfirmation: null
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.createUser(this.state);
+    this.setState({ email: null });
+    this.setState({ password: null });
+    this.setState({ passwordConfirmation: null });
+  };
+
+  handleEmailChange = event => {
+    this.setState({ email: event.target.value });
+  };
+
+  handlePasswordChange = event => {
+    this.setState({ password: event.target.value });
+  };
+
+  handlePasswordConfirmationChange = event => {
+    this.setState({ passwordConfirmation: event.target.value });
+  };
+
   render() {
     return (
       <div>
-        <Form>
+        <Form onSubmit={this.handleSubmit}>
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              onChange={this.handleEmailChange}
+            />
             <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
+              We'll never share your email with anyone else ;)
             </Form.Text>
           </Form.Group>
 
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              onChange={this.handlePasswordChange}
+            />
           </Form.Group>
+
+          <Form.Group controlId="formBasicPasswordConfirmation">
+            <Form.Label>Password Confirmation</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              onChange={this.handlePasswordConfirmationChange}
+            />
+          </Form.Group>
+
           <Button variant="primary" type="submit">
             Submit
           </Button>
@@ -28,5 +72,9 @@ class SignUp extends React.Component {
     );
   }
 }
+
+SignUp.propTypes = {
+  createUser: PropTypes.func.isRequired
+};
 
 export default SignUp;
