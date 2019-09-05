5c3ac52e528322abac5b4d6f68719207701af3e2
hjdr <harry.jdr@gmail.com>
2019-09-05 14:21:47 +0100

    improved functionality & style of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Navbar, Nav, NavDropdown, Form, FormControl  } from 'react-bootstrap';
+import logo from '../style/images/jinder-flame-white.png'
 
 export default class Header extends React.Component {
   render() {
@@ -9,9 +10,9 @@ export default class Header extends React.Component {
         <Navbar expand="lg">
         <Navbar.Brand href="#home">
           <img
-            src="/logo.svg"
-            width="30"
-            height="30"
+            src={logo}
+            width="35"
+            height="45"
             className="d-inline-block align-top"
             alt="Jinder"
           />
@@ -19,19 +20,22 @@ export default class Header extends React.Component {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <Nav.Link style={linkStyle} href="/">Home</Nav.Link>
+              <Nav.Link style={linkStyle} href="/">Sign In</Nav.Link>
+              <Nav.Link style={linkStyle} href="/profiles">View Profiles</Nav.Link>
+              <NavDropdown title={<span style={linkStyle}>Your Profile</span>}>
+                <NavDropdown.Item style={dropdownLinkStyle} href="/profile">View</NavDropdown.Item>
+                <NavDropdown.Item style={dropdownLinkStyle}  href="/profile">Edit</NavDropdown.Item>
+                <NavDropdown.Item style={dropdownLinkStyle}  href="#action/3.3">Something</NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
               </NavDropdown>
+              <Nav.Link style={linkStyle} href="/about">About Us</Nav.Link>
             </Nav>
+            <Nav.Link style={linkStyle} href="#link">Sign Up</Nav.Link>
             <Form inline>
               <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-              <Button variant="outline-success">Search</Button>
+              <Button style={buttonStyle} variant="outline-success">Search</Button>
             </Form>
           </Navbar.Collapse>
         </Navbar>
@@ -42,11 +46,18 @@ export default class Header extends React.Component {
 
 const headerStyle = {
   background: '#FF5903',
-  color: '#fff',
-
 }
 
 const linkStyle = {
-  color: '#fff',
-  textDecoration: 'none'
+  color: '#fff'
+}
+
+const dropdownLinkStyle = {
+  color: '#FF5903'
+}
+
+const buttonStyle = {
+  background: '#fff',
+  border: '#fff',
+  color: '#FF5903'
 }
