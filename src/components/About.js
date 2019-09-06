4a13bf26a8aa1f96944e9f779cfc19565a106471
AnastasiiaBlaha <anastasiia.blaha@gmail.com>
2019-09-06 15:19:24 +0100

    added some styling packages, added email button that works, updated headings and body styling

--- src/components/About.js
+++ src/components/About.js
@@ -1,34 +1,76 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
+import Button from "react-bootstrap/Button";
+import { Form, Label, Input, FormGroup } from 'reactstrap';
+
 
 // import PropTypes from "prop-types";
-// import Button from "react-bootstrap/Button";
 // import Form from "react-bootstrap/Form";
+
+const H1 = styled.h1`
+text-align: center;
+color: #FF5903;
+font-size: 100px;
+`
+const themeSettings = styled.h1`
+    main: 'mediumseagreen';
+`
+
+// const Body = styled.body`
+// text-align: center;
+// color: blue;
+// font-size: 50px;
+
+// &:active,
+//   &:focus {
+//     text-align: left;
+//   }
+// `
+const Body = styled.body`
+    font-size: 16px;
+    border: solid 1px #dbdbdb;
+    border-radius: 3px;
+    color: #262626;
+    padding: 7px 33px;
+    border-radius: 3px;
+    color: #999;
+    cursor: text;
+    font-size: 14px;
+    font-weight: 300;
+    text-align: center;
+    background: #fafafa;
+    &:active,
+    &:focus {
+        text-align: center;
+    }
+`
 
 class About extends React.Component {
 
     render() {
         return (
-            <div style={getStyle}>
+            <div>
                 <header>
-                    <h1 style={headingStyle}>
+                    <ThemeProvider theme={themeSettings}> 
+                    <H1>
                         About Us
-                    </h1>
-                        <body style={textStyle}>We created this web app to make a recruitment process fun, engaging and easy for both job hunter and employers.</body>
-                    <h1 style={headingStyle}>
+                    </H1>
+                    </ThemeProvider>
+                        <Body>We created this web app to make a recruitment process fun, engaging and easy for both job hunter and employers.</Body>
+                    <H1>
                         Our Vision and Mission
-                    </h1>
-                       <body>To make recruitment process fun, engaging and easy.</body>
-                    <h1 style={headingStyle}>
+                    </H1>
+                       <Body>To make recruitment process fun, engaging and easy.</Body>
+                    <H1>
                         Who Are We?
-                    </h1>
-                        <body>We are the cool kids gang.</body>
-                    <h1 style={headingStyle}>
+                    </H1>
+                        <Body>We are the cool kids gang.</Body>
+                    <H1>
                         Contact Us
-                        <body>Email:</body>
-                        <body>Telephone:</body>
-                        <body>Instagram:</body>
-                    </h1>
+                        <Body><Button href="mailto:anastasiia.blaha@gmail.com">Email</Button></Body>
+                        <Body>Telephone:</Body>
+                        <Body>Instagram:</Body>
+                    </H1>
                 </header>
             </div>
         )
@@ -36,17 +78,4 @@ class About extends React.Component {
 }
 
 export default About;
-
-const textStyle= {
-    color: '##FF5903',
-}
-
-const headingStyle= {
-    text: 'center',
-    color: '##FF5903'
-}
-
-const getStyle= {
-    background: '#f4f4f4',
-}
 
