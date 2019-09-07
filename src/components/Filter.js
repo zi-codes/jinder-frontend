d6d34c75df7afadccb62e97e0734db00c9fdb147
Student <ziwei.polaris@gmail.com>
2019-09-07 19:09:29 +0100

    hot keyword search multikeyword onebox

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,36 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Select from "react-select";
+import { industryOptions } from "../data/IndustryData";
+import { skillsOptions } from "../data/SkillsData";
 import { Button, Form, FormControl } from "react-bootstrap";
 
 // wait to integrate with harry ;)
 
 class Filter extends React.Component {
   state = {
-    keywords: []
+    keywords: ""
   };
 
   handleChange = event => {
     let value = event.target.value;
-    let keyword = value.trim().toLowerCase();
-    this.setState({ keywords: [keyword, ...this.state.keywords] }, () => {
+    let keywords = value.split(" ").map(keyword => keyword.toLowerCase());
+    this.setState({ keywords: keywords }, () => {
       this.props.filterCards(this.state.keywords);
     });
   };
 
-  handleSubmit = event => {};
-
   render() {
     return (
       <div>
-        <Form inline onSubmit={this.handleSubmit}>
+        <Form inline>
           <FormControl
             type="text"
             placeholder="Filter by industry or skill"
             className="mr-sm-2"
             onChange={this.handleChange}
           />
-          <Button variant="outline-success">+</Button>
+          <Button variant="outline-success">Filter</Button>
         </Form>
       </div>
     );
