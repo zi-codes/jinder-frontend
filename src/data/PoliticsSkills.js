b4bdcf6c0679b22653b3d9a970270d5b1e538e9b
AnastasiiaBlaha <anastasiia.blaha@gmail.com>
2019-09-12 12:44:24 +0100

    added additional skills to SE, created and added skills to politics industry, created and added skills to badger racer industry

---
+++ src/data/PoliticsSkills.js
@@ -0,0 +1,13 @@
+export const skillsOptions = [
+    { value: 'Public Speaking', label: 'Public Speaking' },
+    { value: 'Crisis Management', label: 'Crisis Management' },
+    { value: 'Persuasion Skills', label: 'Persuasion Skills' },
+    { value: 'Strong Networking Ability', label: 'Strong Networking Ability' },
+    { value: 'Manipulation Skills', label: 'Manipulation Skills' },
+    { value: 'Lying', label: 'Lying' },
+    { value: 'Extortion', label: 'Extortion' },
+    { value: 'Charisma', label: 'Charisma' },
+    { value: 'Communication Skills', label: 'Communication Skills'},
+    { value: 'Fundraising', label: 'Fundraising' },
+    { value: 'Volunteering', label: 'Volunteering' },
+  ];
---
+++ src/data/RacerSkills.js
@@ -0,0 +1,13 @@
+export const skillsOptions = [
+    { value: 'Speed', label: 'Speed' },
+    { value: 'Love of Badgers', label: 'Love of Badgers' },
+    { value: 'Badger Grooming', label: 'Badger Grooming' },
+    { value: 'Maneuverability', label: 'Maneuverability' },
+    { value: 'Badger Training', label: 'Badger Training' },
+    { value: 'Health and Stamina', label: 'Health and Stamina' },
+    { value: 'Course Racing', label: 'Course Racing' },
+    { value: 'Strategic Thinking', label: 'Strategic Thinking' },
+    { value: 'Animal Language', label: 'Animal Language'},
+    { value: 'Critical Strategic Thinking', label: 'Critical Strategic Thinking' },
+    { value: 'Clinical Skills', label: 'Clinical Skills' },
+  ];
